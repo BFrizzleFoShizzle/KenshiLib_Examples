1b7c3cb09f0241ddc0cd186e0eed727275e54c8c
Bryden Frizzell <bryden.frizzell@gmail.com>
2026-06-24 10:13:44 +1200

    Added proper fix to stop highlight applying to portraits

--- CharacterHighlight/CharacterHighlight.cpp
+++ CharacterHighlight/CharacterHighlight.cpp
@@ -8,6 +8,9 @@
 #include <kenshi/PlayerInterface.h>
 #include <kenshi/Faction.h>
 #include <kenshi/FactionRelations.h>
+#include <kenshi/KingOfRenderThread.h>
+#include <kenshi/Renderer.h>
+#include <kenshi/RenderToTextureotron.h>
 #include <kenshi/Appearance.h>
 #include <core/Functions.h>
 #include <ogre/OgreMaterial.h>
@@ -17,7 +20,39 @@
 #include <ogre/OgreSharedPtr.h>
 #include <Debug.h>
 
-bool lastHighlight = false;
+void EnableHighlight(Character* character)
+{
+	// isEnemy if enemy with at least one character
+	bool isEnemy = false;
+	for (int i = 0; i < ou->player->playerCharacters.size() && !isEnemy; ++i)
+		isEnemy = character->isEnemy(ou->player->playerCharacters[i], true);
+	//for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
+	//	isEnemy = (*iter)->shouldIScrewThisGuyOver(thisptr->player->playerCharacters[i]);
+	//for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
+	//	isEnemy = (*iter)->areYouGonnaGetMe(thisptr->player->playerCharacters[i]);
+	for (int i = 0; i < ou->player->playerCharacters.size() && !isEnemy; ++i)
+		isEnemy = ou->player->playerCharacters[i]->areYouGonnaGetMe(character);
+
+	// isAlly if allied with all characters
+	bool isAlly = true;
+	for (int i = 0; i < ou->player->playerCharacters.size() && isAlly; ++i)
+		isAlly = character->isAlly(ou->player->playerCharacters[i], true);
+
+	// if neither, colour based on relations
+	// scale -100-100 to -1-1
+	float factionRelations = (character->getFaction()->relations->getFactionRelation(ou->player->getFaction()) / 100.0f);
+
+	if (character->isPlayerCharacter())
+		character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(0.0, 0.0, 1.0, 1.0));
+	else if (isEnemy)
+		character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(1.0, 0.0, 0.0, 1.0));
+	else if (isAlly)
+		character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(0.0, 1.0, 0.0, 1.0));
+	else
+		character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(std::max(0.0f, -factionRelations + 1.0f), factionRelations + 1, 0.0, 1.0));
+
+	character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getVertexProgramParameters()->setNamedConstant("overrideDepth", true);
+}
 
 void (*GameWorld__mainLoop_GPUSensitiveStuff_orig)(GameWorld* thisptr, float time);
 void GameWorld__mainLoop_GPUSensitiveStuff_hook(GameWorld* thisptr, float time)
@@ -29,36 +64,7 @@ void GameWorld__mainLoop_GPUSensitiveStuff_hook(GameWorld* thisptr, float time)
 		{
 			if (!(*iter)->getAppearance()->bodyMaterial.isNull())
 			{
-				// isEnemy if enemy with at least one character
-				bool isEnemy = false;
-				for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
-					isEnemy = (*iter)->isEnemy(thisptr->player->playerCharacters[i], true);
-				//for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
-				//	isEnemy = (*iter)->shouldIScrewThisGuyOver(thisptr->player->playerCharacters[i]);
-				//for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
-				//	isEnemy = (*iter)->areYouGonnaGetMe(thisptr->player->playerCharacters[i]);
-				for (int i = 0; i < thisptr->player->playerCharacters.size() && !isEnemy; ++i)
-					isEnemy = thisptr->player->playerCharacters[i]->areYouGonnaGetMe((*iter));
-
-				// isAlly if allied with all characters
-				bool isAlly = true;
-				for (int i = 0; i < thisptr->player->playerCharacters.size() && isAlly; ++i)
-					isAlly = (*iter)->isAlly(thisptr->player->playerCharacters[i], true);
-
-				// if neither, colour based on relations
-				// scale -100-100 to -1-1
-				float factionRelations = ((*iter)->getFaction()->relations->getFactionRelation(thisptr->player->getFaction()) / 100.0f);
-
-				if ((*iter)->isPlayerCharacter())
-					(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(0.0, 0.0, 1.0, 1.0));
-				else if (isEnemy)
-					(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(1.0, 0.0, 0.0, 1.0));
-				else if (isAlly)
-					(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(0.0, 1.0, 0.0, 1.0));
-				else
-					(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(std::max(0.0f, - factionRelations + 1.0f), factionRelations + 1, 0.0, 1.0));
-
-				(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getVertexProgramParameters()->setNamedConstant("overrideDepth", true);
+				EnableHighlight(*iter);
 			}
 		}
 	}
@@ -73,16 +79,8 @@ void GameWorld__mainLoop_GPUSensitiveStuff_hook(GameWorld* thisptr, float time)
 				(*iter)->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getVertexProgramParameters()->setNamedConstant("overrideDepth", false);
 			}
 		}
-		// force update portraits (hack to stop portraits being semi-permanently blue)
-		// TODO better fix
-		if (lastHighlight != key->highlight)
-		{
-			const lektor<Character*>& characters = ou->player->getAllPlayerCharacters();
-			for (int i = 0; i < characters.size(); ++i)
-				ou->addPortraitUpdate(characters[i]->handle);
-		}
 	}
-	lastHighlight = key->highlight;
+
 	GameWorld__mainLoop_GPUSensitiveStuff_orig(thisptr, time);
 }
 
@@ -98,10 +96,33 @@ void GameWorld_removeFromUpdateListMain_hook(GameWorld* thisptr, Character* char
 	GameWorld_removeFromUpdateListMain_orig(thisptr, character);
 }
 
+// fix to stop highlight applying to character portraits
+void (*RenderToTextureotron_renderCharacter_orig)(RenderToTextureotron* thisptr, Character* character, Ogre::SharedPtr<Ogre::Texture> texture, const iVector2& size, const Ogre::TRect<float>& rect);
+void RenderToTextureotron_renderCharacter_hook(RenderToTextureotron* thisptr, Character* character, Ogre::SharedPtr<Ogre::Texture> texture, const iVector2& size, const Ogre::TRect<float>& rect)
+{
+	if (key->highlight)
+	{
+		// disable highlight
+		if (!character->getAppearance()->bodyMaterial.isNull())
+		{
+			character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getFragmentProgramParameters()->setNamedConstant("coloroverride", Ogre::ColourValue(1.0, 1.0, 1.0, 0.0));
+			character->getAppearance()->bodyMaterial->getTechnique(0)->getPass(0)->getVertexProgramParameters()->setNamedConstant("overrideDepth", false);
+		}
+	}
+	RenderToTextureotron_renderCharacter_orig(thisptr, character, texture, size, rect);
+	if (key->highlight)
+	{
+		// re-enable highlight
+		EnableHighlight(character);
+	}
+}
+
 __declspec(dllexport) void startPlugin()
 {
 	if (KenshiLib::SUCCESS != KenshiLib::AddHook(KenshiLib::GetRealAddress(&GameWorld::_NV_mainLoop_GPUSensitiveStuff), &GameWorld__mainLoop_GPUSensitiveStuff_hook, &GameWorld__mainLoop_GPUSensitiveStuff_orig))
 		ErrorLog("Character Highlight: could not install hook!");
 	if (KenshiLib::SUCCESS != KenshiLib::AddHook(KenshiLib::GetRealAddress(&GameWorld::removeFromUpdateListMain), &GameWorld_removeFromUpdateListMain_hook, &GameWorld_removeFromUpdateListMain_orig))
+		ErrorLog("Character Highlight: could not install hook!");
+	if (KenshiLib::SUCCESS != KenshiLib::AddHook(KenshiLib::GetRealAddress(&RenderToTextureotron::renderCharacter), &RenderToTextureotron_renderCharacter_hook, &RenderToTextureotron_renderCharacter_orig))
 		ErrorLog("Character Highlight: could not install hook!");
 }
